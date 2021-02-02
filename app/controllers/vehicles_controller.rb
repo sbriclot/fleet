66abfb179af88f5708ed86ef4105047440f7477b
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-02-02 11:55:59 +0100

    Added cloudinary attachment and refactoring of vehicle controller

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,53 +1,56 @@
 class VehiclesController < ApplicationController
-	before_action :set_vehicle, only: [:show, :edit, :update]
+  before_action :set_vehicle, only: %i[show edit update]
 
-	def index
-		@all_vehicles = Vehicle.all     # à supprimer 
-		@vehicles = Vehicle.where(user_id: current_user.id)
-	end
+  def index
+    @all_vehicles = Vehicle.all     # a supprimer
+    @vehicles = Vehicle.where(user_id: current_user.id)
+  end
 
-	def show
-	end
+  def show
+  end
 
-	def new
-		@vehicle = Vehicle.new
-		@action = "Ajouter"
-	end
-	
-	def create
-		@vehicle = Vehicle.new(vehicle_params)
-		user = current_user
-		@vehicle.user = user
+  def new
+    @vehicle = Vehicle.new
+    @action = "Ajouter"
+  end
 
-		if @vehicle.save
-      		redirect_to vehicles_path, notice: "Vehicle successfully created"
-		else
-			@action = "Ajouter"
-      		render :new
-		end
-	end
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    user = current_user
+    @vehicle.user = user
 
-	def edit
-		@action = "Editer"
-	end
+    if @vehicle.save
+      redirect_to vehicles_path, notice: "Vehicle successfully created"
+    else
+      @action = "Ajouter"
+      render :new
+    end
+  end
 
-	def update
-		if @vehicle.update(vehicle_params)
-			@vehicle.save
-			redirect_to vehicles_path, notice: "Vehicle successfully updated"
-		else
-			@action = "Editer"
-			render :edit
-		end
-	end
+  def edit
+    @action = "Editer"
+  end
 
-	private
+  def update
+    if @vehicle.update(vehicle_params)
+      @vehicle.save
+      redirect_to vehicles_path, notice: "Vehicle successfully updated"
+    else
+      @action = "Editer"
+      render :edit
+    end
+  end
 
-	def vehicle_params
-		params.require(:vehicle).permit(:brand, :model, :registration, :sn, :tank_capacity, :actual_km, :purchase_date, :purchase_km, :sale_date, :sale_km)
-	end
-	
-	def set_vehicle
-		@vehicle = Vehicle.find(params[:id])
-	end
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:brand,
+                                    :model, :registration, :sn,
+                                    :tank_capacity, :actual_km, :purchase_date,
+                                    :purchase_km, :sale_date, :sale_km, :photo)
+  end
+
+  def set_vehicle
+    @vehicle = Vehicle.find(params[:id])
+  end
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,4 +2,5 @@ class Invoice < ApplicationRecord
   belongs_to :invoice_type
   belongs_to :vehicle
   belongs_to :shop
+  has_many_attached :docs
 end
