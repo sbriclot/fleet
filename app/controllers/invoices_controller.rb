d329e7c4671b66e7e622a2e95ace5c95b3819f9c
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-02-03 13:50:04 +0100

    Working total Expenses update

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,6 +1,7 @@
 class InvoicesController < ApplicationController
   before_action :set_invoice, only: %i[show edit update]
-  before_action :set_vehicle, only: %i[index new create edit update]
+  before_action :set_vehicle
+  before_action :old_invoice_price, only: %i[update]
 
   def index
     @invoices = Invoice.where(vehicle_id: params[:vehicle_id])
@@ -16,9 +17,9 @@ class InvoicesController < ApplicationController
   end
 
   def create
-    # Ne fonctionne pas car n'est pas valide sans invoice_type_id mais j'ignore comment creer cela pour l'instant
     @invoice = Invoice.new(invoice_params)
     @invoice.vehicle = @vehicle
+    @vehicle.total_expenses += params[:price]
 
     if @invoice.save
       redirect_to vehicle_invoice_path(@vehicle, @invoice), notice: 'Facture créee'
@@ -33,8 +34,10 @@ class InvoicesController < ApplicationController
   end
 
   def update
+    # Si il y a modification du prix, soustrait l'ancien prix et le remplace par le nouveau
+
     if @invoice.update(invoice_params)
-      redirect_to vehicle_invoice_path(@invoice), notice: 'Facture mise à jour'
+      redirect_to vehicle_invoice_path(@vehicle, @invoice), notice: 'Facture mise à jour'
     else
       @action = 'Modifier'
       render :edit
@@ -56,5 +59,9 @@ class InvoicesController < ApplicationController
 
   def set_vehicle
     @vehicle = Vehicle.find(params[:vehicle_id])
+  end
+
+  def old_invoice_price
+    @old_price = @invoice.price
   end
 end
