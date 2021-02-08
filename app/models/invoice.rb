3f29646dceec4971251257bdf5551080f356c228
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-02-08 14:38:23 +0100

    Moved error messages to simple form

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -5,13 +5,9 @@ class Invoice < ApplicationRecord
   has_many_attached :docs
 
   validates :date, presence: true
-  validates :km, presence: { message: 'Veuillez renseigner à quel kilométrage cette facture a été réalisée' },
-                 numericality: {  greater_than: 0,
-                                  message: 'Un véhicule ne peux avoir un kilométrage négatif ou égal à 0' }
-  validates :price, presence: { message: 'Veuillez renseigner un prix' },
-                    numericality: { greater_than: 0, message: 'Un prix ne peux être négatif ou égal à 0' }
-  validates :invoice_type_id, presence: { message: "Veuillez selectionner un type d'opération relatif à cette facture" }
-  validates :invoice_ref, presence: { message: 'Veuillez renseigner la référence de cette facture' },
-                          uniqueness: { message: 'Cette référence existe déjà dans notre base de données' }
-  validates :shop_id, presence: { message: 'Cette facture doit être liée à un garage existant' }
+  validates :km, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :invoice_type_id, presence: true
+  validates :invoice_ref, presence: true, uniqueness: true
+  validates :shop_id, presence: true
 end
