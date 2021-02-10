59820adcdc649991e2f143cc3a9d98b384b77d40
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-02-10 15:21:26 +0100

    Added validation to prevent back from the future invoices

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -6,8 +6,15 @@ class Invoice < ApplicationRecord
 
   validates :invoice_ref, presence: true, uniqueness: true
   validates :date, presence: true
+  validate :invoice_date_cannot_be_in_the_future
   validates :km, presence: true, numericality: { greater_than: 0 }
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :invoice_type_id, presence: true
   validates :shop_id, presence: true
+
+  def invoice_date_cannot_be_in_the_future
+    return unless date.present? && date.strftime > Date.today.strftime
+
+    errors.add(:date, "Une facture ne peut être créée depuis le future")
+  end
 end
