b75fb5ece6546f42e551090496c6bca0adfbdd26
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-02-10 14:49:16 +0100

    Added validations

--- app/models/refuel.rb
+++ app/models/refuel.rb
@@ -1,4 +1,11 @@
 class Refuel < ApplicationRecord
   belongs_to :fuel
   belongs_to :vehicle
+
+  validates :date, presence: true
+  validates :km, presence: true, numericality: { greater_than: 0 }
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :fuel_id, presence: true
+  validates :vehicle_id, presence: true
 end
