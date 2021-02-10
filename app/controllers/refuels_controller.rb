9657eb2e97336632be124a90bffbb10313ad00d6
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-02-10 12:21:11 +0100

    Added basic crud methods and index view

--- app/controllers/refuels_controller.rb
+++ app/controllers/refuels_controller.rb
@@ -1,2 +1,35 @@
 class RefuelsController < ApplicationController
+  before_action :set_refuel, only: %i[create]
+  before_action :find_vehicle, only: %i[index create]
+
+  def index
+    @refuels = Refuel.where(params[:vehicle_id])
+  end
+
+  def new
+    @refuel = Refuel.new
+  end
+
+  def create
+    @refuel.vehicle = @vehicle
+    if @refuel.save
+      redirect_to vehicle_refuels_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @refuel.destroy
+  end
+
+  private
+
+  def set_refuel
+    params(:refuel).permit(:date, :km, :quantity, :price, :fuel_id, :vehicle_id)
+  end
+
+  def find_vehicle
+    @vehicle = Vehicle.find(params[:id])
+  end
 end
