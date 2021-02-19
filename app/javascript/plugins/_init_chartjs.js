d7a0a94e0b3513d5eb7df1e380e0825d98eab45e
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-02-19 14:27:29 +0100

    Added comment to prevent error for now

--- app/javascript/plugins/_init_chartjs.js
+++ app/javascript/plugins/_init_chartjs.js
@@ -1,7 +1,8 @@
 import Chart from "chart.js";
-const ctx = document.querySelector("#refuels-charts").getContext("2d");
-const refuelChart = new Chart(ctx, {
-  type: line,
-});
+// const ctx = document.querySelector("#refuels-charts").getContext("2d");
+// const refuelChart = new Chart(ctx, {
+//   type: line,
+//   // Need to add data
+// });
 
-export { refuelChart };
+// export { refuelChart };
