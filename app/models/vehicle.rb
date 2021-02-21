c53a9c53e77a032dcdc9aeaf501b509ee320b388
Stephane Briclot <stephane.briclot@gmail.com>
2021-02-21 10:37:01 +0100

    Sale date and km validation rules

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -8,4 +8,22 @@ class Vehicle < ApplicationRecord
   has_many :reminders
   has_many :vehicle_fuels
   has_one_attached :photo
+
+  validates :sale_km, numericality: { greater_than: :purchase_km }, allow_nil: true
+  validate :sale_date_after_purchase_date
+  validate :sale_date_cannot_be_in_the_future
+
+  private
+
+  def sale_date_after_purchase_date
+    return if sale_date.blank? || purchase_date.blank?
+
+    errors.add(:sale_date, " ne peut pas être inférieure à la date d'achat") if sale_date < purchase_date
+  end
+
+  def sale_date_cannot_be_in_the_future
+    return unless sale_date.present? && sale_date.strftime > Date.today.strftime
+
+    errors.add(:sale_date, " ne peut pas être dans le futur")
+  end
 end
