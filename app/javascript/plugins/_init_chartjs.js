1e31055dff024f45959b6c426907d5b297e60ff1
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-03-02 16:55:09 +0100

    Switched data to improve front

--- app/javascript/plugins/_init_chartjs.js
+++ app/javascript/plugins/_init_chartjs.js
@@ -13,17 +13,17 @@ const refuelChart = () => {
         labels: dates,
         datasets: [
           {
+            label: "Quantité de carburant",
+            data: quantity,
+            backgroundColor: "rgba(169,169,245, 1)",
+            borderColor: "rgba(49,4,180, 1)",
+            borderWidth: 1,
+          },
+          {
             label: "Prix du plein",
             data: price,
             backgroundColor: "rgba(90,192,77, 0.5)",
             borderColor: "rgba(30,111,20, 1)",
-            borderWidth: 1,
-          },
-          {
-            label: "Quantité de carburant",
-            data: quantity,
-            backgroundColor: "rgba(169,169,245, 0.8)",
-            borderColor: "rgba(49,4,180, 1)",
             borderWidth: 1,
           },
         ],
