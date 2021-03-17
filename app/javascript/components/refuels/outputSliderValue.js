5873a6649ffe060a2919aa59915f29e5b9fb5a0b
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-03-17 16:22:07 +0100

    Removed useless function

--- app/javascript/components/refuels/outputSliderValue.js
+++ app/javascript/components/refuels/outputSliderValue.js
@@ -1,16 +1,14 @@
-const inputSlider = document.querySelector("#refuel-number");
-const outputElement = document.querySelector("#number");
-const titleSpan = document.querySelector("#title-number");
 function outputValue() {
+  const inputSlider = document.querySelector("#refuel-number");
+  const titleSpan = document.querySelector("#title-number");
+  const outputElement = document.querySelector("#number");
   if (inputSlider) {
-    window.addEventListener("load", updateValue);
-    inputSlider.addEventListener("input", updateValue);
+    //window.addEventListener("load", updateValue);
+    inputSlider.addEventListener("input", () => {
+      outputElement.value = inputSlider.value;
+      titleSpan.textContent = inputSlider.value;
+    });
   }
-}
-
-function updateValue() {
-  outputElement.value = inputSlider.value;
-  titleSpan.textContent = inputSlider.value;
 }
 
 export { outputValue };
