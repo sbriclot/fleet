40e4fed67101e4723b1de909453d983166b95eb0
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-03-19 23:19:16 +0100

    fixed output displaying

--- app/javascript/components/refuels/outputSliderValue.js
+++ app/javascript/components/refuels/outputSliderValue.js
@@ -1,7 +1,7 @@
 function outputValue() {
-  const inputSlider = document.querySelector("#refuel-number");
+  const inputSlider = document.querySelector("#refuel-slider");
   const titleSpan = document.querySelector("#title-number");
-  const outputElement = document.querySelector("#number");
+  const outputElement = document.querySelector("#slider-value");
   if (inputSlider) {
     // Need to select the datasets and select the last n elements depending on the slider value
     inputSlider.addEventListener("input", () => {
