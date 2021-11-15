809b034f1a52ef8e1704f326d1e3dec7da8874a1
Jean-Pierre Symoens <jpsymoens0@gmail.com>
2021-11-15 11:42:13 +0100

    Cleaning React setup

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,10 @@ require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
-// Support component names relative to this directory:
-const componentRequireContext = require.context("components", true);
-const ReactRailsUJS = require("react_ujs");
-ReactRailsUJS.useContext(componentRequireContext);
+  // Support component names relative to this directory:
+// const componentRequireContext = require.context("components", true);
+// const ReactRailsUJS = require("react_ujs");
+// ReactRailsUJS.useContext(componentRequireContext);
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
